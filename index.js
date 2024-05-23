6929b5bd9659d9028932c1fc923d91d224a6e33a
Rizki Fauzi <rizkifauzi37@gmail.com>
2024-05-23 17:32:07 +0700

    fix : remove unused component

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/product.model')
 const productRoute = require('./routes/product.route')
 const app = express()
 
